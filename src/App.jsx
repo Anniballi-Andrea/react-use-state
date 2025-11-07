dcb1dd68c7795d7d7c6915509c338fe62448039b
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-07 12:24:35 +0100

    add card component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Card from "./assets/components/Card"
 
 
 function App() {
@@ -21,29 +22,7 @@ function App() {
     <>
 
       <div className="container">
-        <div className="card mt-3">
-          <div className="card-header">
-            <h1>Learn Web development</h1>
-          </div>
-          <div className="card-body">
-
-            <button className="btn btn-primary m-3">html</button>
-            <button className="btn btn-primary m-3">css</button>
-            <button className="btn btn-primary m-3">javaScript</button>
-            <button className="btn btn-primary m-3">Node.js</button>
-            <button className="btn btn-primary m-3">Express</button>
-            <button className="btn btn-primary m-3">ReactJS</button>
-          </div>
-          <div className="container">
-            <h2>HTML</h2>
-            <p>
-              HTML (Hyper Text Markup Language) è il linguaggio standard per creare pagine e applicazioni
-              web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini
-            </p>
-
-
-          </div>
-        </div>
+        <Card />
       </div>
 
     </>
---
+++ src/assets/components/Card.jsx
@@ -0,0 +1,35 @@
+export default function Card() {
+
+
+
+
+
+
+    return (
+
+        <div className="card mt-3">
+            <div className="card-header">
+                <h1>Learn Web development</h1>
+            </div>
+            <div className="card-body">
+
+                <button className="btn btn-primary m-3">html</button>
+                <button className="btn btn-primary m-3">css</button>
+                <button className="btn btn-primary m-3">javaScript</button>
+                <button className="btn btn-primary m-3">Node.js</button>
+                <button className="btn btn-primary m-3">Express</button>
+                <button className="btn btn-primary m-3">ReactJS</button>
+            </div>
+            <div className="container">
+                <h2>HTML</h2>
+                <p>
+                    HTML (Hyper Text Markup Language) è il linguaggio standard per creare pagine e applicazioni
+                    web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini
+                </p>
+
+
+            </div>
+        </div>
+    )
+
+}
