4a58dad32ba8dfc869224d9d7d6be4b19910f4b1
Anniballi-Andrea <andreanniballi@gmail.com>
2025-11-07 17:17:21 +0100

    semplify the code with luigi's help

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,39 @@ import Card from "./assets/components/Card"
 function App() {
 
 
+  const info =
+    [
+      {
+        id: 1,
+        name: "HTML",
+        description: " HTML (Hyper Text Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini"
+      },
+      {
+        id: 2,
+        name: 'CSS',
+        description: "CSS (Cascading Style Sheets) si occupa dell'aspetto grafico gestendo elementi come colori, font, layout e animazioni, rendendo i siti più accattivanti e fruibili su diversi dispositivi.",
+      },
+      {
+        id: 3,
+        name: "JavaScript",
+        description: "JavaScript è un linguaggio di programmazione e scripting che rende le pagine web interattive e dinamiche",
+      },
+      {
+        id: 4,
+        name: "NodeJs",
+        description: "Node.js è  un ambiente di runtime open source che permette di eseguire codice JavaScript al di fuori del browser, principalmente per lo sviluppo di applicazioni server-side"
+      },
+      {
+        id: 5,
+        name: "Express",
+        description: "Express è un framework web per Node.js, leggero, veloce e minimalista, utilizzato per costruire applicazioni web e API. Semplifica lo sviluppo web lato server fornendo una struttura solida e flessibile, gestendo attività come il routing e il middleware in modo efficiente e veloce. ",
+      },
+      {
+        id: 6,
+        name: "ReactJS",
+        description: "ReactJs è una libreria JavaScript open-source per la creazione di interfacce utente (UI), si concentra sulla costruzione di componenti UI riutilizzabili, utilizzando una sintassi simile a HTML chiamata JSX, che viene poi convertita in codice JavaScript. ",
+      },
+    ]
 
 
 
@@ -21,7 +54,7 @@ function App() {
           <div className="card-header">
             <h1>Learn Web development</h1>
           </div>
-          <Card />
+          <Card info={info} />
         </div>
       </div>
 
--- src/assets/components/Card.jsx
+++ src/assets/components/Card.jsx
@@ -4,44 +4,11 @@ import { useState } from "react"
 
 
 
-export default function Card() {
+export default function Card({ info }) {
 
 
-    const [active, setActive] = useState(1)
+    const [active, setActive] = useState(0)
 
-    const info =
-        [
-            {
-                id: 1,
-                name: "HTML",
-                description: " HTML (Hyper Text Markup Language) è il linguaggio standard per creare pagine e applicazioni web. Struttura il contenuto web e fornisce elementi di base come titoli, paragrafi e immagini"
-            },
-            {
-                id: 2,
-                name: 'CSS',
-                description: "CSS (Cascading Style Sheets) si occupa dell'aspetto grafico gestendo elementi come colori, font, layout e animazioni, rendendo i siti più accattivanti e fruibili su diversi dispositivi.",
-            },
-            {
-                id: 3,
-                name: "JavaScript",
-                description: "JavaScript è un linguaggio di programmazione e scripting che rende le pagine web interattive e dinamiche",
-            },
-            {
-                id: 4,
-                name: "NodeJs",
-                description: "Node.js è  un ambiente di runtime open source che permette di eseguire codice JavaScript al di fuori del browser, principalmente per lo sviluppo di applicazioni server-side"
-            },
-            {
-                id: 5,
-                name: "Express",
-                description: "Express è un framework web per Node.js, leggero, veloce e minimalista, utilizzato per costruire applicazioni web e API. Semplifica lo sviluppo web lato server fornendo una struttura solida e flessibile, gestendo attività come il routing e il middleware in modo efficiente e veloce. ",
-            },
-            {
-                id: 6,
-                name: "ReactJS",
-                description: "ReactJs è una libreria JavaScript open-source per la creazione di interfacce utente (UI), si concentra sulla costruzione di componenti UI riutilizzabili, utilizzando una sintassi simile a HTML chiamata JSX, che viene poi convertita in codice JavaScript. ",
-            },
-        ]
 
 
 
@@ -52,28 +19,27 @@ export default function Card() {
 
 
         <div div className="card-body" >
-
-            <button onClick={() => setActive(1)} key={1} className="btn btn-primary m-3">HTML</button>
-            <button onClick={() => setActive(2)} key={2} className="btn btn-primary m-3">CSS</button>
-            <button onClick={() => setActive(3)} key={3} className="btn btn-primary m-3">JavaScript</button>
-            <button onClick={() => setActive(4)} key={4} className="btn btn-primary m-3">NodeJS</button>
-            <button onClick={() => setActive(5)} key={5} className="btn btn-primary m-3">Express</button>
-            <button onClick={() => setActive(6)} key={6} className="btn btn-primary m-3">ReactJS</button>
             {
-
                 info.map((el) => (
-
-                    <>
-                        <div className="container">
-                            {el.id === active && <h2>{el.name}</h2>}
-                            {el.id === active && <p>{el.description}</p>}
-                        </div>
-                    </>
-
-
+                    <button onClick={() => setActive(el.id)} key={el.id} className={`btn btn-primary m-3 ${active === el.id && "btn-warning"}`}>{el.name}</button>
 
                 ))
             }
+
+
+            {active !== 0 && <div className="container">
+
+                {<h2 >{info.find((el) => el.id === active).name}</h2>}
+
+                {<p >{info.find((el) => el.id === active).description}</p>}
+            </div>}
+
+
+
+
+
+
+
 
 
         </div>
